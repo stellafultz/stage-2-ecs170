85febca376d14bf1d961ef29d0bd0be5290e8dd9
kayyayay <kaylahanarinta@gmail.com>
2026-04-24 09:49:26 -0700

    Add files via upload
    Kayla's

--- script/stage_2_script/script_mlp.py
+++ script/stage_2_script/script_mlp.py
@@ -1,47 +1,83 @@
-from local_code.stage_2_code.Dataset_Loader import Dataset_Loader
-from local_code.stage_2_code.Method_MLP import Method_MLP
+from code.stage_2_code.Dataset_Loader import Dataset_Loader
+from code.stage_2_code.Method_MLP import Method_MLP
+from code.stage_2_code.Result_Saver import Result_Saver
+from code.stage_2_code.Setting_Train_Test_Split import Setting_Train_Test_Split
+from code.stage_2_code.Evaluate_Accuracy import Evaluate_Accuracy
+from code.stage_2_code.Evaluate_F1 import Evaluate_F1
+from code.stage_2_code.Evaluate_Precision import Evaluate_Precision
+from code.stage_2_code.Evaluate_Recall import Evaluate_Recall
+
 import numpy as np
-import torch
-import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 
-np.random.seed(2)
-torch.manual_seed(2)
 
-# --- Load Data ---
-train_loader = Dataset_Loader(dName='MNIST_train', dDescription='')
-train_loader.dataset_source_folder_path = 'data/stage_2_data/'
-train_loader.dataset_source_file_name = 'train.csv'
-X_train, y_train = train_loader.load()
+# -------------------------
+# MLP Script (Stage 2)
+# -------------------------
+if __name__ == "__main__":
 
-test_loader = Dataset_Loader(dName='MNIST_test', dDescription='')
-test_loader.dataset_source_folder_path = 'data/stage_2_data/'
-test_loader.dataset_source_file_name = 'test.csv'
-X_test, y_test = test_loader.load()
+    np.random.seed(1)
 
-print(f'Train: {X_train.shape}, Test: {X_test.shape}')
+    # 1. Dataset
+    data_obj = Dataset_Loader('mnist', '')
+    data_obj.dataset_source_folder_path = '../../data/stage_2_data/'
 
-# --- Train ---
-method_obj = Method_MLP(mName='MLP', mDescription='')
-method_obj.train(X_train, y_train)
+    # 2. Model (MLP using PyTorch)
+    method_obj = Method_MLP('mlp', '')
 
-# --- Predict ---
-y_pred = method_obj.test(X_test)
+    # 3. Result saver
+    result_obj = Result_Saver('saver', '')
+    result_obj.result_destination_folder_path = '../../result/stage_2_result/'
+    result_obj.result_destination_file_name = 'mlp_result'
 
-# --- Evaluate ---
-print('\n===== Results =====')
-print('Accuracy:            ', accuracy_score(y_test, y_pred))
-print('F1 (weighted):       ', f1_score(y_test, y_pred, average='weighted'))
-print('Recall (weighted):   ', recall_score(y_test, y_pred, average='weighted'))
-print('Precision (weighted):', precision_score(y_test, y_pred, average='weighted'))
+    # 4. Evaluation
+    evaluate_obj = Evaluate_Accuracy('accuracy', '')
 
-# --- Convergence Curve ---
-plt.figure()
-plt.plot(range(1, method_obj.max_epoch+1), method_obj.loss_history, marker='o')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.title('MLP Training Convergence Curve')
-plt.grid(True)
-plt.savefig('../../result/stage_2_result/MLP_convergence_curve.png')
-plt.show()
+    # 5. Setting (IMPORTANT: NO K-FOLD)
+    setting_obj = Setting_Train_Test_Split('train test split', '')
 
+    print("************ Start ************")
+
+    setting_obj.prepare(data_obj, method_obj, result_obj, evaluate_obj)
+    setting_obj.print_setup_summary()
+
+    mean_score, result= setting_obj.load_run_save_evaluate()
+
+    data = {
+        'pred_y': result['pred_y'],
+        'true_y': result['true_y']
+    }
+
+    f1_eval = Evaluate_F1('f1', '')
+    precision_eval = Evaluate_Precision('precision', '')
+    recall_eval = Evaluate_Recall('recall', '')
+
+    f1_eval.data = data
+    precision_eval.data = data
+    recall_eval.data = data
+
+    print("F1:", f1_eval.evaluate())
+    print("Precision:", precision_eval.evaluate())
+    print("Recall:", recall_eval.evaluate())
+
+    print("************ Overall Performance ************")
+    print("MLP Accuracy:", mean_score)
+
+    import matplotlib.pyplot as plt
+
+    plt.figure()
+    plt.plot(result['loss_list'])
+    plt.title("Loss Curve")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.savefig("/Users/kayyayay/Downloads/ECS189G_Winter_2022_Source_Code_Template/result/stage_2_result/loss_curve.png")
+    plt.close()
+
+    plt.figure()
+    plt.plot(result['acc_list'])
+    plt.title("Accuracy Curve")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.savefig("/Users/kayyayay/Downloads/ECS189G_Winter_2022_Source_Code_Template/result/stage_2_result/accuracy_curve.png")
+    plt.close()
+
+    print("************ Finish ************")
